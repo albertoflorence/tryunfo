b234f1dfbf4c4efe9cade3cbd2beb40982b8d846
albertoflorence <alberto.sabatier@gmail.com>
2023-05-11 22:19:47 -0300

    add LightEllipse component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Main from './components/Main';
 import Header from './components/Header/Header';
+import LightEllipse from './components/LightEllipse/LightEllipse';
 
 export default function App() {
   return (
-    <div>
+    <>
+      <LightEllipse
+        style={ {
+          left: '-30%',
+          top: '-50%',
+        } }
+      />
+      <LightEllipse
+        style={ {
+          right: '-32%',
+          top: '18%',
+        } }
+      />
       <Header />
       <Main />
-    </div>
+    </>
   );
 }
---
+++ src/components/LightEllipse/LightEllipse.jsx
@@ -0,0 +1,7 @@
+import './lightEllipse.css';
+
+export default function LightEllipse(props) {
+  return (
+    <div className="light-ellipse" { ...props } />
+  );
+}
